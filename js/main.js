bc38fc8e5afe0a7477e21a41f032581101d2c97a
Miguel Romero <porfirio.romero112@gmail.com>
2021-04-24 17:49:02 -0400

    Arreglo de botones y agregado de estilos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// CAPTURA DE TIPO DE CALCULADORA ========================================================================================================================
+const CONTENT_ALL = document.getElementsByClassName('content-all')
+const SELECT = document.getElementById('select-calculator')
+SELECT.addEventListener('change', ()=> types(SELECT.value))
+const types = (value)=>{
+    if(value === 'estandar'){
+        CONTENT_ALL[0].removeAttribute('hidden')
+    } else if (value === 'cientifica'){
+        CONTENT_ALL[0].setAttribute("hidden", "hidden")
+    }
+} // Muestra la calculadora segun el tipo, si es cientifico crea un nuevo div
+
+
 
 // CAPTURA DE BOTONES========================================================================================================================
 
@@ -11,12 +24,29 @@ const display = document.getElementById('result')
 
 // MANEJO DEL DISPLAY ========================================================================================================================
 
+const listButton = {
+    decimals: '.'
+}
+
+const TYPES_OPERATOR = {
+    plus: '+',
+    minus: '-',
+    multiply: '*',
+    divide: '/',
+    pow: '^',
+    square: '√'
+}
+
 const NOT_VALUE = ''
 let currentValue = NOT_VALUE;
 const MESSAGE_ERROR = {
-        syntaxError : 'ERROR! Falta una expesion', 
+        syntaxError : 'ERROR! Falta una expesion',
+        operatorError: 'ERROR! No hay argumentos',
         mathError: 'ERROR! La division por cero no existe'
 }
+
+
+// VALIDACIONES ========================================================================================================================
 
 // EVENTOS DE CADA BOTON ========================================================================================================================
 
@@ -44,9 +74,8 @@ const deleteOne = () => {
 }// Elimina de a uno los numeros que estan en la pantalla
 
 const addNumber =(number)=>{
-    if(currentValue.includes('.')){
-        if (number === '.') return
-    }
+    display.setAttribute('class', 'result')
+    if(currentValue.includes(listButton.decimals)) if (number === listButton.decimals) return
     currentValue = currentValue.toString() + number.toString()
     display.value = currentValue
 } // Agrega numeros a la pantalla, los muestra
@@ -61,49 +90,55 @@ const saveValues = (operator) =>{
 const solveOperation = ()=>{
     /* Agrega las otras operaciones como suma resta multiplicacion
 dvision, potenciacion ect */
-    if(typeOperation === '√'){
-        n = parseFloat(previousValue)
-        getResult = Math.sqrt(n)
-        display.value = getResult
-        currentValue = getResult
-        return
+    if(typeOperation === TYPES_OPERATOR.square){
+        if(previousValue === ''){
+            display.setAttribute('class', 'input-error')
+            display.value = MESSAGE_ERROR.operatorError
+            currentValue = NOT_VALUE
+            previousValue = NOT_VALUE
+        }else{
+            oneNumber = parseFloat(previousValue)
+            getResult = Math.sqrt(oneNumber)
+            display.value = getResult
+            currentValue = getResult
+            return
+        }
     }
         firstNumber =  parseFloat(previousValue)
         secondNumber = parseFloat(currentValue)
-        if(isNaN(firstNumber) || isNaN(secondNumber)){
+        if((isNaN(firstNumber) && isNaN(secondNumber))  ){
+            display.setAttribute('class', 'input-error')
+            display.value = MESSAGE_ERROR.operatorError
+            currentValue = NOT_VALUE
+            previousValue = NOT_VALUE
+            return
+        } else if(isNaN(firstNumber) || isNaN(secondNumber)){
+            display.setAttribute('class', 'input-error')
             display.value = MESSAGE_ERROR.syntaxError
             currentValue = NOT_VALUE
             previousValue = NOT_VALUE
             return
-        } else
-        {
+        }
+        else {
         switch (typeOperation) {
-            case '+':
-                getResult = firstNumber + secondNumber
-                break;
-            case '-':
-                getResult = firstNumber - secondNumber
-                break;
-            case '*':
-                getResult = firstNumber * secondNumber
-                break;
-            case '/':
-                if(secondNumber === 0){
-                    getResult = MESSAGE_ERROR.mathError
-                    display.value = NOT_VALUE;
+            case TYPES_OPERATOR.plus: getResult = firstNumber + secondNumber; 
+            break;
+            case TYPES_OPERATOR.minus: getResult = firstNumber - secondNumber; 
+            break;
+            case TYPES_OPERATOR.multiply: getResult = firstNumber * secondNumber ; 
+            break;
+            case TYPES_OPERATOR.divide:  
+            if(secondNumber === 0){
+                display.setAttribute('class', 'input-error')
+                getResult = MESSAGE_ERROR.mathError
+                display.value = NOT_VALUE;
                 } else{
-                    getResult = firstNumber / secondNumber
+                getResult = firstNumber / secondNumber
                 }
                 break;
-            case '^':
-                getResult = Math.pow(firstNumber, secondNumber)
-                break;
-            // case '√':
-            //     getResult = Math.sqrt(secondNumber)
-            //     break;
-            default:
-                getResult = "Sintax ERROR"
-                break;
+            case TYPES_OPERATOR.pow: 
+            getResult = Math.pow(firstNumber, secondNumber); break;
+            default: getResult = "Sintax ERROR"; break;
         }
     }
         display.value = getResult;
@@ -115,22 +150,9 @@ dvision, potenciacion ect */
 const clearScreen = () =>{
     previousValue = NOT_VALUE;
     currentValue = NOT_VALUE;
+    display.setAttribute('class', 'result')
     display.value = NOT_VALUE;
 }
 
-
 // OPERACIONES CON UN SOLO ARGUMENTO ==========================================================
-
-// const unArgumento = (operador)=> {
-//     if(operador == '^'){ 
-//     getResult = Math.sqrt(currentValue)
-//     display.value = getResult;
-//     currentValue = getResult;
-//     }
-
-// }
-
-
-// FUNCIONES TRIGONOMETRICAS ==========================================================
-
-// const funcionesTrignometricas 
+// FUNCIONES TRIGONOMETRICAS ==================================================================
